b1e7b26374cec12e07fa277a691eb59c5be172ec
Viktor Rocha <viktoorrocha@gmail.com>
2017-09-20 19:15:53 -0400

    Adding some more views

--- app/src/main/java/br/com/layoutkotlin/atmconsultoria/DetalheEmpresaActivity.kt
+++ app/src/main/java/br/com/layoutkotlin/atmconsultoria/DetalheEmpresaActivity.kt
@@ -3,12 +3,10 @@ package br.com.layoutkotlin.atmconsultoria
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DetalheEmpresaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        
+        setContentView(R.layout.activity_detalhe_empresa)
     }
 }
