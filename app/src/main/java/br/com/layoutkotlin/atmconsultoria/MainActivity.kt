52063978d4b453ab40d105ceaed95ef747adccbd
Viktor Rocha <viktoorrocha@gmail.com>
2017-09-20 20:25:42 -0400

    Finalizando app

--- app/src/main/java/br/com/layoutkotlin/atmconsultoria/MainActivity.kt
+++ app/src/main/java/br/com/layoutkotlin/atmconsultoria/MainActivity.kt
@@ -1,7 +1,10 @@
 package br.com.layoutkotlin.atmconsultoria
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +12,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+
+        button_contato.setOnClickListener {
+            val intent = Intent(this, DetalheContatoActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_empresa.setOnClickListener {
+            val intent = Intent(this, DetalheEmpresaActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_servicos.setOnClickListener {
+            val intent = Intent(this, DetalheServicosActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_cliente.setOnClickListener {
+            val intent = Intent(this, DetalheClientesActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
